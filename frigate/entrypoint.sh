9a95200a0827b5618d7aee023e3f06e5b7b8179e
Pete Muir <pmuir@bleepbleep.org.uk>
2023-10-27 23:59:11 +0100

    fix filenames

--- frigate/entrypoint.sh
+++ frigate/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tempio -conf /data/options.json -template /proxy.conf.gtpl -out /etc/nginx/conf.d/proxy.conf
+tempio -conf /data/options.json -template /nginx.conf.gtpl -out /etc/nginx/conf.d/proxy.conf
 nginx -T -c  /etc/nginx/conf.d/proxy.conf
 
 exec /docker-entrypoint.sh
